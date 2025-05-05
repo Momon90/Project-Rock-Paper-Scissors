9d46b8a2577a086cde2facf19c5eca8042724e93
Momon90 <206674436+Momon90@users.noreply.github.com>
2025-05-05 21:27:43 +0530

    Done with step 2

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Project-Rock-Paper-Scissors
+
+PSEUDOCODE
+
+Step: 1
+Understand the problem
+- create a new function named getCoumputerChoice
+- hint: Math.random static method 
+- arrays aren't necessary here.
+
+Plan
+- input: the input will be from Math.random method or from the computer or idk
+- Desired output: random string values: 'rock', 'paper' or 'scissors' from the function itself
+
+Pseudocode 
+- when the input comes in, in this case if from Math.random
+- if input = 0. output = rock
+- if input = 1. output = paper
+- if input = 2. output = scissor
+
+Subproblem: how to let Rock be returned if Math.floor(Math.random() * 3) return 0 using conditional statement. - ans: put it in a variable const randomNumber = Math.floor(Math.random() * max);
--- script.js
+++ script.js
@@ -1,60 +1,9 @@
 function getComputerChoice(max = 3) {
-    const randomNumber = Math.floor(Math.random() * max)
-    if (randomNumber === 2) {
-        return 'Rock';
-    } else if (randomNumber === 1) {
-        return 'Paper';
-    } else if (randomNumber === 0) {
-        return 'Scissor';
-    }
+    const randomNumber = Math.floor(Math.random() * max);   if (randomNumber === 0) {
+    return `Rock`;
+   } else if (randomNumber === 1) {
+    return `Paper`;
+   } else {
+    return `Scissors`;
+   }
 }
-
-function getHumanChoice() {
-    const inputValue = prompt('Enter Rock, Paper or Scissor');
-    if (inputValue.toLowerCase() === 'Rock'.toLowerCase()) {
-        return 'Rock';
-    } else if (inputValue.toLowerCase() === 'Paper'.toLowerCase()) {
-        return 'Paper';
-    } else if (inputValue.toLowerCase() === 'Scissor'.toLowerCase()) {
-        return 'Scissor';
-    }
-}
-
-function playGame() {
-
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice == 'Rock' && computerChoice == 'Rock') {
-            console.log('Neutral');
-        } else if (humanChoice === 'Rock' && computerChoice === 'Paper') {
-            console.log('You lose! Paper beats Rock');
-        } else if (humanChoice === 'Rock' && computerChoice === 'Scissor') {
-            console.log('You win!');
-        } else if (humanChoice === 'Paper' && computerChoice === 'Rock') {
-            console.log('You win!');
-        } else if (humanChoice === 'Paper' && computerChoice === 'Paper') {
-            console.log('Neutral');
-        } else if (humanChoice === 'Paper' && computerChoice === 'Scissor') {
-            console.log('You lose! Scissor beats Paper');
-        } else if (humanChoice === 'Scissor' && computerChoice === 'Rock') {
-            console.log('You lose! Rock beats Scissor');
-        } else if (humanChoice === 'Scissor' && computerChoice === 'Paper') {
-            console.log('You win!');
-        } else if (humanChoice === 'Scissor' && computerChoice === 'Scissor') {
-            console.log('Neutral');
-        }
-    }
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    const result1 = playRound(humanSelection, computerSelection);
-}
-
-playGame();
-playGame();
-
-
-
-
-
